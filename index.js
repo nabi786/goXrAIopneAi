84c8981398c4dffe091e4af95d24e2db3616cc22
nabi786 <rehanpardesi2018@gmail.com>
2023-02-25 18:50:38 +0500

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv-flow").config();
 require("./routes/middlewares/mongo");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(cors());
